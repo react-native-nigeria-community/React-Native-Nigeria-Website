c03ba2cc4f657b36d6b0fbfcc61bf20e99926a2e
Victory-n <ndukwevictory@gmail.com>
2025-09-16 11:45:32 +0100

    Created the newsletter section and added global.node to the eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default defineConfig([
             globals: {
                 ...globals.browser,
                 ...globals.jest,
+                ...globals.node,
             },
             parserOptions: {
                 ecmaVersion: 'latest',
---
+++ src/components/sections/newsletter.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import Button from "../common/button.jsx";
+import InputField from "../common/input-field.jsx";
+import Typography from "../common/typography.jsx";
+
+const Newsletter = () => {
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!email.includes("@")) {
+            setError("Please enter a valid email address.");
+        } else {
+            setError("");
+            alert(`Subscribed with: ${email}`);
+            setEmail("");
+        }
+    };
+
+    return (
+        <section className="w-full lg:flex lg:justify-center py-12">
+            <div className="text-center px-4 lg:px-\\\[24px] lg:py-\\\[77px]">
+                {/* Heading */}
+                <Typography as="h3" variant="h3" responsiveVariant={{ lg: "h4" }}>
+                    Subscribe to our newsletter
+                </Typography>
+
+                {/* Subheading */}
+                <Typography as="h6" variant="h6" className={"mt-2 mb-6 text-h6 lg:text-p"} responsiveVariant={{ lg: "p" }}>
+                    Stay updated with events, resources, and the latest in React Native.
+                </Typography>
+
+
+                {/* Form */}
+                <form onSubmit={handleSubmit}>
+                    <div className="space-y-6 lg:space-y-0 lg:flex lg:items-center lg:border-1 lg:border-bd-secondary/50  lg:rounded-sm lg:px-[24px] lg:py-[16px]">
+                        <InputField
+                            id="email"
+                            type="email"
+                            className={"w-full p-3 lg:w-lg h-10 focus:outline-none border border-black rounded-[10px] lg:border-none"}
+                            placeholder="Email address"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            error={error}
+                        />
+                        <div className="">
+                            <Button type="submit" className="w-full py-3" variant="primary" onClick={handleSubmit}>
+                                Subscribe
+                            </Button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default Newsletter;
