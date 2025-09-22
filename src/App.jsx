b993fa265631de83ecfb43275752246ff60d5d52
Victory-n <ndukwevictory@gmail.com>
2025-09-23 00:04:24 +0100

    Updated the App.jsx file to reflect all button style types

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import ButtonComponent from "./components/commons/button.jsx";
 
 function App() {
 
+    const handleClick = () => {
+        alert("Button clicked!");
+    };
+
     return (
-        <ButtonComponent>Join Community</ButtonComponent>
+        <div className="bg-black w-full flex flex-col gap-4 items-center p-10 justify-center">
+            <ButtonComponent onClick={handleClick} variant="primary">Join Community</ButtonComponent>
+            <ButtonComponent onClick={handleClick} variant="secondary">RSVP</ButtonComponent>
+            <ButtonComponent onClick={handleClick} variant="accent">View Calendar</ButtonComponent>
+        </div>
     );
 }
 
