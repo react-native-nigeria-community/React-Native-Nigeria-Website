1c097003f147df0455affe511555bf0703067978
Victory-n <ndukwevictory@gmail.com>
2025-09-22 14:08:43 +0100

    Complete project configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
-import './App.css'
-import Accordion from "./components/common/accordion.jsx";
+import './App.css';
 
 function App() {
 
-    const accordionItems = [
-        {
-            title: "Who can join the community?",
-            content: "Yes, joining the community is completely free. We believe in open knowledge sharing and collaboration.",
-        },
-        {
-            title: "Is membership free?",
-            content: "Yes, joining the community is completely free. We believe in open knowledge sharing and collaboration.",
-        },
-        {
-            title: " Do I need prior experience with React Native?",
-            content: "No, beginners are welcome. The community offers resources and mentorship to help you get started.",
-        },
-        {
-            title: "How do I get updates on events and activities?",
-            content: "No, beginners are welcome. The community offers resources and mentorship to help you get started.",
-        },
-        {
-            title: "Can I showcase my React Native projects?",
-            content: "No, beginners are welcome. The community offers resources and mentorship to help you get started.",
-        },
-    ];
-
     return (
-        <div className="">
-            <Accordion items={accordionItems} allowMultiple={false} />
-        </div>
+        <></>
     );
 }
 
