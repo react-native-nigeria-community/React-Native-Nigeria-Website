5c39747057aa853a41502864743a1815a387d42e
Victory-n <ndukwevictory@gmail.com>
2025-09-22 23:44:53 +0100

    Created the button component and test file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import ButtonComponent from "./components/commons/button.jsx";
 
 function App() {
 
     return (
-        <></>
+        <ButtonComponent>Join Community</ButtonComponent>
     );
 }
 
