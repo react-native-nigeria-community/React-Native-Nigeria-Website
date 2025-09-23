809607fa01d35819f8c589437655f302b9f9afb1
Victory-n <ndukwevictory@gmail.com>
2025-09-23 09:37:04 +0100

    Updated the App.jsx file to reflect new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import './App.css';
-import ButtonComponent from "./components/commons/button.jsx";
+import AccordionComponent from "./components/commons/accordion.jsx";
 
 function App() {
 
-    const handleClick = () => {
-        alert("Button clicked!");
-    };
+    const accordionItems = [
+        {
+            title: "Who can join the community?",
+            content: "Yes, joining the community is completely free. We believe in open knowledge sharing and collaboration.",
+        },
+        {
+            title: "Is membership free?",
+            content: "Yes, joining the community is completely free. We believe in open knowledge sharing and collaboration.",
+        },
+        {
+            title: " Do I need prior experience with React Native?",
+            content: "No, beginners are welcome. The community offers resources and mentorship to help you get started.",
+        },
+        {
+            title: "How do I get updates on events and activities?",
+            content: "No, beginners are welcome. The community offers resources and mentorship to help you get started.",
+        },
+        {
+            title: "Can I showcase my React Native projects?",
+            content: "No, beginners are welcome. The community offers resources and mentorship to help you get started.",
+        },
+    ];
 
     return (
-        <div className="bg-black w-full flex flex-col gap-4 items-center p-10 justify-center">
-            <ButtonComponent onClick={handleClick} variant="primary">Join Community</ButtonComponent>
-            <ButtonComponent onClick={handleClick} variant="secondary">RSVP</ButtonComponent>
-            <ButtonComponent onClick={handleClick} variant="accent">View Calendar</ButtonComponent>
+        <div>
+            <AccordionComponent items={accordionItems} allowMultiple={false} />
         </div>
     );
 }
