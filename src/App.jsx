624169e0f846acf8928e4e4d07a6fd89e4c4f5b1
Victory-n <ndukwevictory@gmail.com>
2025-09-26 08:49:17 +0100

    Created the newsletter feature component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import NewsletterComponent from "./components/layouts/newsletter.jsx";
+
+function App() {
+
+    return (
+        <NewsletterComponent />
+    );
+}
+
+export default App;
