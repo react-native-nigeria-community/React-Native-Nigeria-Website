f779abedf2948c51f62ee1b03b88fdf0e786020b
Victory-n <45641923+Victory-n@users.noreply.github.com>
2025-09-26 15:41:59 +0100

    Merge branch 'main' into ORTHO-CU-869am814f_Create-the-navbar-component-for-BWRN

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import NavbarComponent from "./components/layouts/navbar.jsx";
+
+function App() {
+
+    return (
+        <NavbarComponent />
+    );
+}
+
+export default App;
