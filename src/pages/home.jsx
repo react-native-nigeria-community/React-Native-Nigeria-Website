22332e87b30561b7d06abd2e691f2409baab9578
Victory-n <ndukwevictory@gmail.com>
2025-09-28 18:59:36 +0100

    Updated the home page to display content

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,1 +1,13 @@
-//Created the home page
+import TypographyComponent from "../components/commons/typography.jsx";
+
+function HomePage () {
+    return (
+        <div className={"mx-auto"}>
+            <TypographyComponent as={"h1"} variant={"h1"}>
+                Welcome to the Home Page
+            </TypographyComponent>
+        </div>
+    )
+}
+
+export default HomePage;
