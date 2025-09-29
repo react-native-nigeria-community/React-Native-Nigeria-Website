ce16931828f99c86254c1f06a61ec1cc5a6a77f6
Victory-n <ndukwevictory@gmail.com>
2025-09-29 16:56:01 +0100

    Added the navbar component to the hope page

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import NavbarComponent from "../components/layouts/navbar.jsx";
+
+function HomePage () {
+    return (
+        <>
+            <NavbarComponent />
+        </>
+    )
+}
+
+export default HomePage;
