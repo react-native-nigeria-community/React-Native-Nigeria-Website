9eda17d2b73b98a06e1eaef511b100f57cd90056
Victory-n <45641923+Victory-n@users.noreply.github.com>
2025-09-29 11:45:09 +0100

    Merge branch 'main' into ORTHO-CU-869amc4rp_Create-footer-component-for-BWRN

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import FooterComponent from "./components/layouts/footer.jsx";
+
+function App() {
+
+    return (
+        <FooterComponent />
+    );
+}
+
+export default App;
