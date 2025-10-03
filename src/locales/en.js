bbd231b4c477aafe8365fb8ecb7aa4bb4bad366e
Victory-n <ndukwevictory@gmail.com>
2025-10-03 15:57:09 +0100

    Created the en.js file and added all strings in home page

---
+++ src/locales/en.js
@@ -0,0 +1,161 @@
+export default {
+
+    // navigation strings
+    nav1: "Home",
+    nav2: "About",
+    nav3: "Event",
+
+    // button strings
+    joinCommunity: "Join Community",
+    explore: "Explore",
+    viewCalendar: "View Calendar",
+    subscribe: "Subscribe",
+
+    // extra strings
+    newsletterTitle: "Subscribe to our newsletter",
+    newsletterDescription: "Stay updated with events, resources, and the latest in React Native.",
+    inputEmail: "Email",
+    placeholderEmail: "Email address",
+    copyRight: "© 2025 React Native Nigeria. All rights reserved",
+    privacyPolicy: "Privacy & Policy",
+    termsConditions: "Terms & Conditions",
+
+    /*
+    ---------------------------------------------------------------------------
+    ALL LANDING PAGE STRINGS
+    ---------------------------------------------------------------------------
+    */
+
+    // first section
+    firstSection: {
+        reactNnc: "React Native Nigeria Community",
+        learnShareConnect: {
+            learnShareConnect1: "Learn. Share. Connect.",
+            learnShareConnect2: "Learn. Share. Connect. Elevate your React Native journey.",
+        },
+    },
+
+    // second section
+    secondSection: {
+        whyRnn: "Why React Native Nigeria",
+        chosenByDevs: "Chosen by developers who care about performance,",
+        communityTalks: {
+            title: "Community talks & pro tips",
+            description: "Learn from real experiences, expert tips, and smarter ways to build with React Native.",
+        },
+        communityEvents: {
+            title: "Community events & meetups",
+            description: "Connect, learn, and grow through our events, talks, and local meetups. Whether online or offline.",
+        },
+        realWorldProject: {
+            title: "Real-world project showcases",
+            description: "Explore real apps built by our community, with insights into how they were made.",
+        },
+        performanceTips: {
+            title: "Performance tips that go deep",
+            description: "Discover advanced techniques to make your React Native apps faster and smoother.",
+        },
+        readyToBuildRna: {
+            title: "Ready to build your next React Native app?",
+            description: "Launch your next React Native project with us, gain insights, support, and tools from a vibrant developer community.",
+        },
+    },
+
+    // third section
+    thirdSection: {
+        featuredTopics: "Featured Topics/Insight",
+        featuredTopicsDescription: "Explore hands-on tips, lessons, and tools from the React Native developer community.",
+        medium: {
+            title: "Medium",
+            description1: "Getting started with Tailwind:  Tailwind to create a button",
+            description2: "I know Tech is the Future but how do i get started",
+            description3: "Using TypeScript effectively for cleaner and scalable apps.",
+        },
+    },
+
+    // fourth section
+    fourthSection: {
+        upComingEvents: {
+            title: "Upcoming Community Events",
+            description: "Stay updated on our monthly live sessions, hackathons, and IRL meetups",
+        },
+        eventCards: {
+            eventCard1: {
+                smallText: "LIVE TALK",
+                title: "Starting a Reactnative project 2020",
+                date: "August 25, 2020",
+                time: "10:00 AM",
+                buttonText: "Watch Live Talk",
+            },
+            eventCard2: {
+                smallText: "MEETUP",
+                title: "React Native Abuja Meetup",
+                date: "September 20, 2025",
+                time: "10:30 AM",
+                buttonText: "RSVP",
+            },
+            eventCard3: {
+                smallText: "EVENT",
+                title: "Build with React Native",
+                date: "October 3rd, 2025",
+                time: "06:00 PM",
+                buttonText: "Register",
+            },
+        },
+    },
+
+    // fifth section
+    fifthSection: {
+        reactNnc: "React Native Nigeria Community",
+        exploreReactNative: "Explore React Native Open Source Projects",
+        sourceProject: {
+            project1: {
+                title: "React Native Nigeria",
+                description: "The official open source website for React Native Nigeria.",
+            },
+            project2: {
+                title: "Rave by Flutterwave",
+                description: "Accept payments effortlessly within your React Native app.",
+            },
+            project3: {
+                title: "Quidpay Payments",
+                description: "Effortless payment collection with zero linking.",
+            },
+            project4: {
+                title: "RN Number Counter",
+                description: "Smooth animated number counter for React Native.",
+            },
+            project5: {
+                title: "Cloudinary Image Picker",
+                description: "Quickly select and upload images directly to Cloudinary.",
+            },
+            project6: {
+                title: "React Native HEXShade",
+                description: "Generate lighter or darker tones instantly.",
+            },
+        },
+    },
+
+    // sixth section
+    sixthSection: {
+        contributeCommunity: {
+            title: "Contribute to the Community",
+            description: "Share your knowledge, inspire others, and help grow the React Native ecosystem.",
+        },
+        communityCard: {
+            communityCard1: {
+                title: "Write for Us",
+                description: "Share your knowledge, inspire others, and grow your voice in the React Native community.",
+            },
+            communityCard2: {
+                title: "Host a Talk",
+                description: "Share your insight, experience, or ideas with the community , on stage or online.",
+            },
+            communityCard3: {
+                title: "Share Projects",
+                description: "Built something cool? Share it, get feedback, and connect with fellow devs.",
+            },
+        },
+    },
+
+};
