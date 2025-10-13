21e1f4b033aecbeec45d13984f6fc0b8403d4843
Victory-n <ndukwevictory@gmail.com>
2025-10-13 07:58:22 +0100

    Fixed the hover issue with the footer component

--- src/components/commons/nav-list.jsx
+++ src/components/commons/nav-list.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavList = ({label, link}) => {
     return (
-        <li className="px-auto py-2.5 text-nowrap hover:text-bg2 hover:font-semibold">
+        <li className="px-auto py-2.5 text-nowrap hover:text-primary hover:font-semibold">
             <a href={link}>{label}</a>
         </li>
     );
