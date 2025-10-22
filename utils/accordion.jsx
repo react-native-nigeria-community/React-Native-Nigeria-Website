9c53928c807eb58a4e6ed8480d6726e38fd1e8a0
Sammy fx <sajimoko2@gmail.com>
2025-10-22 04:32:06 +0100

    Fix Past Event section alignement

---
+++ utils/accordion.jsx
@@ -0,0 +1,22 @@
+export const getAccordions = [
+        {
+            title: "Who can join the community?",
+            content: "Yes, joining the community is completely free. We believe in open knowledge sharing and collaboration.",
+        },
+        {
+            title: "Is membership free?",
+            content: "Yes, joining the community is completely free. We believe in open knowledge sharing and collaboration.",
+        },
+        {
+            title: " Do I need prior experience with React Native?",
+            content: "No, beginners are welcome. The community offers resources and mentorship to help you get started.",
+        },
+        {
+            title: "How do I get updates on events and activities?",
+            content: "No, beginners are welcome. The community offers resources and mentorship to help you get started.",
+        },
+        {
+            title: "Can I showcase my React Native projects?",
+            content: "No, beginners are welcome. The community offers resources and mentorship to help you get started.",
+        },
+    ];
---
+++ utils/write-for-us.jsx
@@ -0,0 +1,16 @@
+import en from "../src/locales/en";
+
+export const getWriteUps = [
+        {
+            title: en.sixthSection.communityCard.communityCard1.title,
+            description: en.sixthSection.communityCard.communityCard1.description,
+        },
+        {
+            title: en.sixthSection.communityCard.communityCard2.title,
+            description: en.sixthSection.communityCard.communityCard2.description,
+        },
+        {
+            title: en.sixthSection.communityCard.communityCard3.title,
+            description: en.sixthSection.communityCard.communityCard3.description,
+        },
+    ];
