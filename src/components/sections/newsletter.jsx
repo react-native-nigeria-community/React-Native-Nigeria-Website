6e62a73da5e9a899033a63c9173b54d23e2faa1d
Sammy fx <sajimoko2@gmail.com>
2025-10-22 04:44:57 +0100

    Correct About us text

---
+++ src/components/sections/newsletter.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import ButtonComponent from "../commons/button.jsx";
+import TypographyComponent from "../commons/typography.jsx";
+import InputFieldComponent from "../commons/input-field.jsx";
+import en from "../../locales/en.js";
+
+const NewsletterComponent = () => {
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!email.includes("@")) {
+            setError("Please enter a valid email address.");
+        } else {
+            setError("");
+            alert(`Subscribed with: ${email}`);
+            setEmail("");
+        }
+    };
+
+    return (
+        <section className="w-full lg:flex lg:justify-center py-12">
+            <div className="text-center px-4 lg:px-[24px] lg:py-[77px]">
+                {/* Heading */}
+                <TypographyComponent as="h3" variant="h3" responsiveVariant={{ lg: "h4" }} className={"text-secondary!"}>
+                    {en.newsletterTitle}
+                </TypographyComponent>
+
+                {/* Subheading */}
+                <TypographyComponent as="h6" variant="h6" className={"text-secondary! mt-2 mb-6 text-h6 lg:text-p"} responsiveVariant={{ lg: "p" }}>
+                    {en.newsletterDescription}
+                </TypographyComponent>
+
+
+                {/* Form */}
+                <form onSubmit={handleSubmit}>
+                    <div className="space-y-6 lg:space-y-0 lg:flex lg:items-center lg:border-1 lg:border-bd-secondary/50  lg:rounded-sm lg:px-[24px] lg:py-[16px]">
+                        <InputFieldComponent
+                            id="email"
+                            type="email"
+                            className={"w-full p-3 lg:w-lg h-10 focus:outline-none border border-black rounded-[10px] lg:border-none"}
+                            placeholder={en.placeholderEmail}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            error={error}
+                        />
+                        <div className="">
+                            <ButtonComponent type="submit" className="w-full py-3" variant="primary" onClick={handleSubmit}>
+                                {en.subscribe}
+                            </ButtonComponent>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default NewsletterComponent;
---
+++ src/locales/en.js
@@ -0,0 +1,271 @@
+export default {
+
+    // navigation strings
+    navLogoText: "React Native Nigeria",
+    navHome: "Home",
+    navAbout: "About",
+    navEvent: "Event",
+    navContact: "Contact",
+
+    // button strings
+    joinCommunity: "Join Community",
+    explore: "Explore",
+    viewCalendar: "View Calendar",
+    registerNext: "Register for Next",
+    subscribe: "Subscribe",
+
+    // extra strings
+    sendMessage: "Send Message",
+    checkBoxText: "Keep me up to date on news and events",
+    newsletterTitle: "Subscribe to our newsletter",
+    newsletterDescription: "Stay updated with events, resources, and the latest in React Native.",
+    inputEmail: "Email",
+    placeholderEmail: "Email address",
+    labelName: "Full Name",
+    labelEmail: "Email Address",
+    labelMessage: "Message",
+    copyRight: "React Native Nigeria. All rights reserved",
+    privacyPolicy: "Privacy & Policy",
+    termsConditions: "Terms & Conditions",
+    writeHere: "Write here...",
+
+    /***************************************
+     ---------------------------------------------------------------------------
+     ALL LANDING PAGE STRINGS
+     ---------------------------------------------------------------------------
+     ****************************************/
+
+    // first section
+    firstSection: {
+        reactNnc: "React Native Nigeria Community",
+        reactBrn: "Build with React Native",
+        learnShareConnect: {
+            learnShareConnect1: "Learn. Share. Connect.",
+            learnShareConnect2: "Learn. Share. Connect. Elevate your React Native journey.",
+        },
+    },
+
+    // second section
+    secondSection: {
+        whyRnn: "Why React Native Nigeria",
+        chosenByDevs1: "Chosen by developers who care about performance,",
+        chosenByDevs2: "community, and impact",
+        communityTalks: {
+            title: "Community talks & pro tips",
+            description: "Learn from real experiences, expert tips, and smarter ways to build with React Native.",
+        },
+        communityEvents: {
+            title: "Community events & meetups",
+            description: "Connect, learn, and grow through our events, talks, and local meetups. Whether online or offline.",
+        },
+        realWorldProject: {
+            title: "Real-world project showcases",
+            description: "Explore real apps built by our community, with insights into how they were made.",
+        },
+        performanceTips: {
+            title: "Performance tips that go deep",
+            description: "Discover advanced techniques to make your React Native apps faster and smoother.",
+        },
+        readyToBuildRna: {
+            title: "Ready to build your next React Native app?",
+            description: "Launch your next React Native project with us, gain insights, support, and tools from a vibrant developer community.",
+        },
+    },
+
+    // third section
+    thirdSection: {
+        featuredTopics: "Featured Topics/Insight",
+        featuredTopicsDescription1: "Explore key lessons, tools, and techniques shared by the community.",
+        featuredTopicsDescription2: "Explore hands-on tips, lessons, and tools from the React Native developer community.",
+        medium: {
+            title: "Medium",
+            description1: "Getting started with Tailwind:  Tailwind to create a button",
+            description2: "I know Tech is the Future but how do i get started",
+            description3: "Using TypeScript effectively for cleaner and scalable apps.",
+        },
+    },
+
+    // fourth section
+    fourthSection: {
+        upComingEvents: {
+            title: "Upcoming Community Events",
+            description: {
+                description1: "Stay updated on our monthly live sessions, hackathons, and IRL meetups",
+                description2: "Stay updated on our monthly live sessions, hackathons,",
+                description3: "and IRL meetups",
+            },
+        },
+        eventCards: {
+            eventCard1: {
+                smallText: "LIVE TALK",
+                title: "Starting a Reactnative project 2020",
+                date: "August 25, 2020",
+                time: "10:00 AM",
+                buttonText: "Watch Live Talk",
+            },
+            eventCard2: {
+                smallText: "MEETUP",
+                title: "React Native Abuja Meetup",
+                date: "September 20, 2025",
+                time: "10:30 AM",
+                buttonText: "RSVP",
+            },
+            eventCard3: {
+                smallText: "EVENT",
+                title: "Build with React Native",
+                date: "October 3rd, 2025",
+                time: "06:00 PM",
+                buttonText: "Register",
+            },
+        },
+    },
+
+    // fifth section
+    fifthSection: {
+        reactNnc1: "BUILD WITH REACT NATIVE",
+        reactNNC2: "React Native Nigeria Community",
+        exploreReactNative: "Explore React Native Open Source Projects",
+        sourceProject: {
+            project1: {
+                star: "1",
+                fork: "1",
+                title: "React Native Nigeria",
+                description: "The official open source website for React Native Nigeria.",
+            },
+            project2: {
+                star: "13",
+                fork: "11",
+                title: "Rave by Flutterwave",
+                description: "Accept payments effortlessly within your React Native app.",
+            },
+            project3: {
+                star: "4",
+                fork: "4",
+                title: "Quidpay Payments",
+                description: "Effortless payment collection with zero linking.",
+            },
+            project4: {
+                star: "5",
+                fork: "2",
+                title: "RN Number Counter",
+                description: "Smooth animated number counter for React Native.",
+            },
+            project5: {
+                star: "16",
+                fork: "5",
+                title: "Cloudinary Image Picker",
+                description: "Quickly select and upload images directly to Cloudinary.",
+            },
+            project6: {
+                star: "19",
+                fork: "3",
+                title: "React Native HEXShade",
+                description: "Generate lighter or darker tones instantly.",
+            },
+        },
+    },
+
+    // sixth section
+    sixthSection: {
+        contributeCommunity: {
+            title: "Contribute to the Community",
+            description: "Share your knowledge, inspire others, and help grow the React Native ecosystem.",
+        },
+        communityCard: {
+            communityCard1: {
+                title: "Write for Us",
+                description: "Share your knowledge, inspire others, and grow your voice in the React Native community.",
+            },
+            communityCard2: {
+                title: "Host a Talk",
+                description: "Share your insight, experience, or ideas with the community , on stage or online.",
+            },
+            communityCard3: {
+                title: "Share Projects",
+                description: "Built something cool? Share it, get feedback, and connect with fellow devs.",
+            },
+        },
+    },
+
+
+    /***************************************
+     ---------------------------------------------------------------------------
+     ALL ABOUT-US PAGE STRINGS
+     ---------------------------------------------------------------------------
+     ****************************************/
+
+    aboutPage: {
+        heroText1: "About Us : Powered by Passion,  React Native Nigeria.",
+        heroText2: "React Native Nigeria, is where mobile creators connect, learn, and grow. From beginners to pros, our community shares insights, builds real projects, and explores the latest in React Native. Together, we’re not just coding apps, we’re shaping the future of mobile.",
+        missionStatement: "Our mission is to connect people who are building with React Native, share real-world experiences, and provide resources that help members grow their skills. Whether you’re a beginner taking your first steps in mobile development or an experienced professional pushing the boundaries of what’s possible, you’ll find support, inspiration, and collaboration here. Through shared projects, discussions, tutorials, and events, we aim to empower developers to bring their ideas to life, solve challenges together, and contribute to the global React Native ecosystem.",
+        faq: "FAQ",
+        faqTitle: "Quick answers to common questions about our community.",
+    },
+
+
+    /**
+     ---------------------------------------------------------------------------
+     ALL CONTACT PAGE STRINGS
+     ---------------------------------------------------------------------------
+     **/
+
+    contactPage: {
+        titleText: "Contact us",
+        titleDescription: "Have questions or ideas? We’d love to hear from you.",
+    },
+
+    /**
+     ---------------------------------------------------------------------------
+     ALL EVENTS PAGE STRINGS
+     ---------------------------------------------------------------------------
+     **/
+
+    eventPage: {
+        firstSection: {
+            title1: "Build with React Native Events",
+            title2: "React Native Community Events",
+            slogan: "Learn, connect, and grow with our community.",
+        },
+        secondSection: {
+            title: "React Native Nigeria Abuja Meetup",
+            date: "Saturday 20th September 2025",
+            time: "11.00AM",
+            location: "The bunker, 3 Atbara Street, off Cairo Street. Ademola Adetokunbo",
+            collaborationText: "Collaborate, learn, and connect with React Native developers in Nigeria. 🚀 Talks, hands-on sessions & networking await!",
+            hostText: "Meet the host",
+            xText: "@ReactNativeNG",
+            gitText: "react-native-nigeria",
+        },
+        thirdSection: {
+            title: "Upcoming Events",
+            event1: {
+                title: "Build with React Native",
+                date: "Friday 3rd October 2025",
+                time: "6:00PM",
+                location: "Manchester",
+            },
+        },
+        fourthSection: {
+            title: "Past Events & Highlights",
+            event1: {
+                title: "Starting a React Native Project in 2020",
+                date: "3rd October 2020",
+                time: "10.00AM",
+                location: "Online",
+            },
+            event2: {
+                title: "After Create React App, What's Next?",
+                date: "5th November 2020",
+                time: "2:00PM",
+                location: "Online",
+            },
+            event3: {
+                title: "React Native Community Akure",
+                date: "18th December 2021",
+                time: "9:00AM",
+                location: "Akure",
+            },
+        },
+    }
+
+};
