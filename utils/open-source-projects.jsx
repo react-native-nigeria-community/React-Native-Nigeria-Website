1b2740d78bfa544c9c7a04d6b12aadb7692ec953
Sammy fx <sajimoko2@gmail.com>
2025-11-06 08:52:32 +0100

    Update contact.jsx

---
+++ utils/open-source-projects.jsx
@@ -0,0 +1,39 @@
+import en from "../src/locales/en";
+
+export const getOpenSourceProjects = [
+        {
+            star: en.fifthSection.sourceProject.project1.star,
+            fork: en.fifthSection.sourceProject.project1.fork,
+            title: en.fifthSection.sourceProject.project1.title,
+            description: en.fifthSection.sourceProject.project1.description,
+        },
+        {
+            star: en.fifthSection.sourceProject.project2.star,
+            fork: en.fifthSection.sourceProject.project2.fork,
+            title: en.fifthSection.sourceProject.project2.title,
+        },
+        {
+            star: en.fifthSection.sourceProject.project3.star,
+            fork: en.fifthSection.sourceProject.project3.fork,
+            title: en.fifthSection.sourceProject.project3.title,
+            description: en.fifthSection.sourceProject.project3.description,
+        },
+        {
+            star: en.fifthSection.sourceProject.project4.star,
+            fork: en.fifthSection.sourceProject.project4.fork,
+            title: en.fifthSection.sourceProject.project4.title,
+            description: en.fifthSection.sourceProject.project4.description,
+        },
+        {
+            star: en.fifthSection.sourceProject.project5.star,
+            fork: en.fifthSection.sourceProject.project5.fork,
+            title: en.fifthSection.sourceProject.project5.title,
+            description: en.fifthSection.sourceProject.project5.description,
+        },
+        {
+            star: en.fifthSection.sourceProject.project6.star,
+            fork: en.fifthSection.sourceProject.project6.fork,
+            title: en.fifthSection.sourceProject.project6.title,
+            description: en.fifthSection.sourceProject.project6.description,
+        },
+    ];
