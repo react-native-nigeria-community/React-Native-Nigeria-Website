a44409ce7cfa89ac86ae74d73ed760f0c5f30b25
fisayo~ <101174144+fee-sah-yor@users.noreply.github.com>
2025-11-12 08:41:39 +0100

    fix reload

---
+++ utils/live-events.jsx
@@ -0,0 +1,25 @@
+import en from "../src/locales/en";
+
+export const getLiveEvents = [
+        {
+            label: en.fourthSection.eventCards.eventCard1.smallText,
+            title: en.fourthSection.eventCards.eventCard1.title,
+            date: en.fourthSection.eventCards.eventCard1.date,
+            time: en.fourthSection.eventCards.eventCard1.time,
+            buttonText: en.fourthSection.eventCards.eventCard1.buttonText,
+        },
+        {
+            label: en.fourthSection.eventCards.eventCard2.smallText,
+            title: en.fourthSection.eventCards.eventCard2.title,
+            date: en.fourthSection.eventCards.eventCard2.date,
+            time: en.fourthSection.eventCards.eventCard2.time,
+            buttonText: en.fourthSection.eventCards.eventCard2.buttonText,
+        },
+        {
+            label: en.fourthSection.eventCards.eventCard3.smallText,
+            title: en.fourthSection.eventCards.eventCard3.title,
+            date: en.fourthSection.eventCards.eventCard3.date,
+            time: en.fourthSection.eventCards.eventCard3.time,
+            buttonText: en.fourthSection.eventCards.eventCard3.buttonText,
+        },
+    ];
