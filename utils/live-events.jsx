a4bdf3cdf2c902f4103d76713e5bbba97a0f0ed3
Sammy fx <155273742+Sammyfx4u@users.noreply.github.com>
2025-11-18 21:56:23 +0100

    newletter and contact update

---
+++ utils/live-events.jsx
@@ -0,0 +1,28 @@
+import en from "../src/locales/en";
+
+export const getLiveEvents = [
+        {
+            label: en.fourthSection.eventCards.eventCard1.smallText,
+            title: en.fourthSection.eventCards.eventCard1.title,
+            date: en.fourthSection.eventCards.eventCard1.date,
+            time: en.fourthSection.eventCards.eventCard1.time,
+            buttonText: en.fourthSection.eventCards.eventCard1.buttonText,
+            buttonLink: en.fourthSection.eventCards.eventCard1.buttonLink,
+        },
+        {
+            label: en.fourthSection.eventCards.eventCard2.smallText,
+            title: en.fourthSection.eventCards.eventCard2.title,
+            date: en.fourthSection.eventCards.eventCard2.date,
+            time: en.fourthSection.eventCards.eventCard2.time,
+            buttonText: en.fourthSection.eventCards.eventCard2.buttonText,
+             buttonLink: en.fourthSection.eventCards.eventCard2.buttonLink,
+        },
+        {
+            label: en.fourthSection.eventCards.eventCard3.smallText,
+            title: en.fourthSection.eventCards.eventCard3.title,
+            date: en.fourthSection.eventCards.eventCard3.date,
+            time: en.fourthSection.eventCards.eventCard3.time,
+            buttonText: en.fourthSection.eventCards.eventCard3.buttonText,
+            buttonLink: en.fourthSection.eventCards.eventCard3.buttonLink,
+        },
+    ];
