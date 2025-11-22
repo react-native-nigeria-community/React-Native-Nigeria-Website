4537f224ce1e90052b5ca9a758fa3df027f358d6
Sammy fx <155273742+Sammyfx4u@users.noreply.github.com>
2025-11-22 15:58:12 +0100

    Merge branch 'main' into feature/CU-869b63375-fix-mobile-responsiveness

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home.jsx";
+import AboutUs from "./pages/about-us.jsx";
+import Contact from "./pages/contact.jsx";
+import NavbarComponent from "./components/layouts/navbar.jsx";
+import FooterComponent from "./components/layouts/footer.jsx";
+import NewsletterComponent from "./components/sections/newsletter.jsx";
+import Events from "./pages/event.jsx";
+import ScrollToTop from "./components/ScrollToTop";
+
+function App() {
+    return (
+        <>
+         <ScrollToTop />
+            <NavbarComponent />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about-us" element={<AboutUs />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/events" element={<Events />} />
+            </Routes>
+            <NewsletterComponent />
+            <FooterComponent />
+        </>
+    );
+}
+
+export default App;
