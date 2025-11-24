08e46341d6c649e25c0755e7afc907039d444380
Sammy fx <155273742+Sammyfx4u@users.noreply.github.com>
2025-11-24 06:53:19 +0100

    fix web crash

---
+++ utils/fourth-event-list.jsx
@@ -0,0 +1,22 @@
+import en from "../src/locales/en";
+
+export const fourthEventList = [
+    {
+        title: en.eventPage.fourthSection.event1.title,
+        date: en.eventPage.fourthSection.event1.date,
+        time: en.eventPage.fourthSection.event1.time,
+        location: en.eventPage.fourthSection.event1.location,
+    },
+    {
+        title: en.eventPage.fourthSection.event2.title,
+        date: en.eventPage.fourthSection.event2.date,
+        time: en.eventPage.fourthSection.event2.time,
+        location: en.eventPage.fourthSection.event2.location,
+    },
+    {
+        title: en.eventPage.fourthSection.event3.title,
+        date: en.eventPage.fourthSection.event3.date,
+        time: en.eventPage.fourthSection.event3.time,
+        location: en.eventPage.fourthSection.event3.location,
+    },
+];
