7b4a27b07be2696eb6ae3a7bb11756c8bd8d949f
Sammy fx <155273742+Sammyfx4u@users.noreply.github.com>
2025-11-28 05:17:56 +0100

    Update featured-card.jsx

---
+++ utils/write-for-us.jsx
@@ -0,0 +1,19 @@
+import en from "../src/locales/en";
+
+export const getWriteUps = [
+        {
+            title: en.sixthSection.communityCard.communityCard1.title,
+            description: en.sixthSection.communityCard.communityCard1.description,
+            link: en.sixthSection.communityCard.communityCard1.link,
+        },
+        {
+            title: en.sixthSection.communityCard.communityCard2.title,
+            description: en.sixthSection.communityCard.communityCard2.description,
+            link: en.sixthSection.communityCard.communityCard2.link,
+        },
+        {
+            title: en.sixthSection.communityCard.communityCard3.title,
+            description: en.sixthSection.communityCard.communityCard3.description,
+            link: en.sixthSection.communityCard.communityCard3.link,
+        },
+    ];
