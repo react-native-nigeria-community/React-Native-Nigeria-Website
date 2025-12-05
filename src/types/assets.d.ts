51d34d03f5cc4580a0e20e3bca484a4b9d5fda4b
Oluwatobi Shokunbi <ayoshokz@gmail.com>
2025-12-05 14:12:37 +0000

    fix failing CI

--- src/types/assets.d.ts
+++ src/types/assets.d.ts
@@ -1,19 +1,19 @@
-// declare module "*.svg" {
-//     const content: string;
-// }
+declare module "*.svg" {
+    const content: string;
+}
 
-// declare module "*.png" {
-//     const content: string;
-// }
+declare module "*.png" {
+    const content: string;
+}
 
-// declare module "*.jpg" {
-//     const content: string;
-// }
+declare module "*.jpg" {
+    const content: string;
+}
 
-// declare module "*.jpeg" {
-//     const content: string;
-// }
+declare module "*.jpeg" {
+    const content: string;
+}
 
-// declare module "*.gif" {
-//     const content: string;
-// }
+declare module "*.gif" {
+    const content: string;
+}
