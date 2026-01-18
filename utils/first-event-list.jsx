19b5fa118012a18f93e24789feeaa77f66efdb30
Oluwatobi Shokunbi <ayoshokz@gmail.com>
2026-01-18 20:48:08 +0000

    fix animation on home header

---
+++ utils/first-event-list.jsx
@@ -0,0 +1,14 @@
+import en from "../src/locales/en";
+import xSVG from "../src/assets/svg/x.svg";
+import GitSVG from "../src/assets/svg/git.svg";
+
+export const firstEventList = [
+    {
+        image: xSVG,
+        text: en.eventPage.secondSection.xText,
+    },
+    {
+        image: GitSVG,
+        text: en.eventPage.secondSection.gitText,
+    },
+];
