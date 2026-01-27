36c6b582eadefc895ab010574ed000de4fda389a
Kamssy <kamsicindy424@gmail.com>
2026-01-27 14:20:45 +0100

    extract newsletter strings into i18n and simplify newsletters constant

---
+++ utils/accordion.jsx
@@ -0,0 +1,75 @@
+
+
+export const getAccordions = (t) => [
+  {
+    title: t?.faqAccordion?.whoCanJoin?.title,
+    content: {
+      type: "text",
+      value: t?.faqAccordion?.whoCanJoin?.content,
+    },
+  },
+  {
+    title: t?.faqAccordion?.isMembershipFree?.title,
+    content: {
+      type: "text",
+      value: t?.faqAccordion?.isMembershipFree?.content,
+    },
+  },
+  {
+    title: t?.faqAccordion?.needPriorExperience?.title,
+    content: {
+      type: "text",
+      value: t?.faqAccordion?.needPriorExperience?.content,
+    },
+  },
+  {
+    title: t?.faqAccordion?.getUpdates?.title,
+    content: {
+      type: "jsx",
+      value: () => (
+        <p>
+          {t?.faqAccordion?.getUpdates?.content}{" "}
+          <a
+            href={t?.faqAccordion?.getUpdates?.newsletter?.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 hover:underline"
+          >
+            {t?.faqAccordion?.getUpdates?.newsletter?.text}
+          </a>
+          ,{" "}
+          <a
+            href={t?.faqAccordion?.getUpdates?.slack?.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 hover:underline"
+          >
+            {t?.faqAccordion?.getUpdates?.slack?.text}
+          </a>
+          ,{" "}
+          <a
+            href={t?.faqAccordion?.getUpdates?.whatsapp?.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 hover:underline"
+          >
+            {t?.faqAccordion?.getUpdates?.whatsapp?.text}
+          </a>
+          {" "}{t?.faqAccordion?.getUpdates?.andFollowing}
+        </p>
+      ),
+    },
+  },
+  {
+    title: t?.faqAccordion?.showcaseProjects?.title,
+    content: {
+      type: "links",
+      value: [
+        {
+          label: t?.faqAccordion?.showcaseProjects?.guideline?.label,
+          url: t?.faqAccordion?.showcaseProjects?.guideline?.url,
+        },
+      ],
+    },
+  },
+];
---
+++ utils/nav-links.jsx
@@ -0,0 +1,22 @@
+export const getNavLinks = (t) => [
+    {
+        label: t?.navHome,
+        link: "/",
+    },
+    {
+        label: t?.navAbout,
+        link: "/about-us",
+    },
+    {
+        label: t?.navEvent,
+        link: "/events",
+    },
+    {
+        label: t?.navNewsletter,
+        link: "/newsletters"
+    },
+    {
+        label: t?.navContact,
+        link: "/contact",
+    },
+];
