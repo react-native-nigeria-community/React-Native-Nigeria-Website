f6925c4d6fcc60838e4333b169a1b9f5cebadfb2
Davemafy <imafidondavid1@gmail.com>
2026-03-17 20:44:55 +0100

    Fixed invisible text

---
+++ src/pages/event.jsx
@@ -0,0 +1,122 @@
+import { TypeAnimation } from "react-type-animation";
+import TypographyComponent from "../components/commons/typography.jsx";
+import PastEventHighLights from "../components/commons/past-events.jsx";
+import { getPastEvents, getUpcomingEvents } from "../../utils/event-details";
+import { useTranslation } from "../context/useTranslation.jsx";
+import UpcomingEventHighLights from "../components/commons/upcoming-events.jsx";
+
+function Events() {
+  const { t } = useTranslation();
+
+  const upcomingEvents = getUpcomingEvents()
+  const pastEvents = getPastEvents()
+  
+  return (
+    <>
+      {/* First section */}
+      <section
+        className={
+          "lg:bg-bg1 bg-[url(./assets/img/mb-blue.png)] lg:bg-[url(./assets/img/lg-bg-screen.png)] bg-no-repeat bg-bottom bg-contain h-[366px] lg:h-[655px] flex items-center justify-center text-center lg:w-full"
+        }
+      >
+        <div className={"gird justify-items-center content-center"}>
+          <div
+            className={
+              "px-6 space-y-3 lg:w-[758px] text-secondary lg:text-primary"
+            }
+          >
+            <p className="font-semibold leading-none lg:leading[60px] lg:font-medium text-[40px] tracking-[-0.125rem] md:text-h1">
+              {/* Mobile view */}
+              <span className={"lg:hidden"}>
+                {t.eventPage.firstSection.title1}
+              </span>
+
+              {/* Desktop view */}
+              <TypeAnimation
+                key={t.eventPage.firstSection.title2}
+                sequence={[t.eventPage.firstSection.title2]}
+                speed={50}
+                repeat={1}
+                cursor={false}
+                className={"hidden lg:block"}
+                preRenderFirstString={false}
+              />
+            </p>
+            <p
+              className={
+                "leading-5 text-secondary lg:text-primary tracking-normal text-[16px] lg:text-[14px]"
+              }
+            >
+              {t.eventPage.firstSection.slogan}
+            </p>
+          </div>
+        </div>
+      </section>
+      {/* Second section */}
+      <section className={"px-6 py-12 space-y-6 lg:bg-bg1"}>
+        <div className={"lg:flex lg:justify-center lg:mx-[125px]"}>
+          <div className={"py-6 w-full"}>
+            <TypographyComponent
+              as={"h3"}
+              variant={"h3"}
+              className={"text-mh3! text-4  xl lg:text-primary"}
+            >
+              {t.eventPage.thirdSection.title}
+            </TypographyComponent>
+
+            <div
+              className={
+                "py-6 grid md:grid-cols-2 xl:grid-cols-3 gap-6 justify-items-stretch items-stretch"
+              }
+            >
+              {upcomingEvents.map((list) => (
+                <UpcomingEventHighLights
+                  key={list.id}
+                  id={list.id}
+                  title={list.title}
+                  date={list.date}
+                  time={list.time}
+                  location={list.location}
+                  description={list.description}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Third section */}
+      <section className={"px-6 py-12 space-y-6"}>
+        <div className={"lg:flex lg:justify-center lg:mx-[125px]"}>
+          <div className={"py-6"}>
+            <TypographyComponent
+              as={"h3"}
+              variant={"h3"}
+              className={"text-mh3!"}
+            >
+              {t.eventPage.fourthSection.title}
+            </TypographyComponent>
+
+            <div
+              className={
+                "py-6 grid md:grid-cols-2 xl:grid-cols-3 gap-6 justify-items-stretch items-stretch"
+              }
+            >
+              {pastEvents.map((list) => (
+                <PastEventHighLights
+                  key={list.id}
+                  id={list.id}
+                  title={list.title}
+                  date={list.date}
+                  time={list.time}
+                  location={list.location}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Events;
---
+++ utils/upcoming-event-highlights.jsx
@@ -0,0 +1,19 @@
+import en from "../src/locales/en";
+import CalendarSVG from "../src/assets/svg/calendar.svg";
+import ClockSVG from "../src/assets/svg/clock.svg";
+import MarkerSVG from "../src/assets/svg/marker.svg";
+
+export const upcomingEventHighlights = [
+    {
+        title: CalendarSVG,
+        text: en.eventPage.thirdSection.event1.date,
+    },
+    {
+        image: ClockSVG,
+        text: en.eventPage.thirdSection.event1.time,
+    },
+    {
+        image: MarkerSVG,
+        text: en.eventPage.thirdSection.event1.location,
+    },
+];
