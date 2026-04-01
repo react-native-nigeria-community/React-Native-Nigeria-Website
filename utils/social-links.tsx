2ed11c6f97f6bad171dddcf9993d7577f82e8a7e
fisayo~ <101174144+fee-sah-yor@users.noreply.github.com>
2026-04-01 10:15:15 +0100

    update details card

---
+++ utils/social-links.tsx
@@ -0,0 +1,38 @@
+import TwitterSVG from "../src/assets/svg/twitter.svg";
+import LinkedInSVG from "../src/assets/svg/linkedIn.svg";
+import SlackSVG from "../src/assets/svg/slack.svg";
+import GitHubSVG from "../src/assets/svg/github.svg";
+import YouTubeSVG from "../src/assets/svg/youtube.svg";
+interface Social {
+  image: string;
+  altText: string;
+  link: string;
+}
+
+export const getSocials: Social[] = [
+        {
+            image: LinkedInSVG,
+            altText: "LinkedIn-icon",
+            link: "https://www.linkedin.com/company/react-native-nigeria-community/",
+        },
+        {
+            image: GitHubSVG,
+            altText: "GitHub-icon",
+            link: "https://github.com/react-native-nigeria-community",
+        },
+        {
+            image: TwitterSVG,
+            altText: "Twitter-icon",
+            link: "https://x.com/ReactNativeNG",
+        },
+        {
+            image: YouTubeSVG,
+            altText: "Youtube-icon",
+            link: "https://www.youtube.com/@Reactnativenigeria",
+        },
+        {
+            image: SlackSVG,
+            altText: "Slack-icon",
+            link: "https://reactnativenigeria.slack.com/join/shared_invite/zt-c8od4rkl-xUOp01GMiKOt_7_mFvyOzA#/shared-invite/email",
+        },
+    ];
